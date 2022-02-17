b15174b5581db6fa72fe8841dc8dc15eeb5b5a75
monsterkodi <monsterkodi@gmx.net>
2022-02-17 18:31:44 +0100

    online

--- js/online.js
+++ js/online.js
@@ -1,6 +1,6 @@
 // monsterkodi/kode 0.237.0
 
-var _k_ = {list: function (l) {return l != null ? typeof l.length === 'number' ? l : [] : []}, noon: function (obj) { var pad = function (s, l) { while (s.length < l) { s += ' ' }; return s }; var esc = function (k, arry) { var es, sp; if (0 <= k.indexOf('\n')) { sp = k.split('\n'); es = sp.map(function (s) { return esc(s,arry) }); es.unshift('...'); es.push('...'); return es.join('\n') } if (k === '' || k === '...' || _k_.in(k[0],[' ','#','|']) || _k_.in(k[k.length - 1],[' ','#','|'])) { k = '|' + k + '|' } else if (arry && /  /.test(k)) { k = '|' + k + '|' }; return k }; var pretty = function (o, ind, seen) { var k, kl, l, v, mk = 4; if (Object.keys(o).length > 1) { for (k in o) { if (Object.hasOwn(o,k)) { kl = parseInt(Math.ceil((k.length + 2) / 4) * 4); mk = Math.max(mk,kl); if (mk > 32) { mk = 32; break } } } }; l = []; var keyValue = function (k, v) { var i, ks, s, vs; s = ind; k = esc(k,true); if (k.indexOf('  ') > 0 && k[0] !== '|') { k = `|${k}|` } else if (k[0] !== '|' && k[k.length - 1] === '|') { k = '|' + k } else if (k[0] === '|' && k[k.length - 1] !== '|') { k += '|' }; ks = pad(k,Math.max(mk,k.length + 2)); i = pad(ind + '    ',mk); s += ks; vs = toStr(v,i,false,seen); if (vs[0] === '\n') { while (s[s.length - 1] === ' ') { s = s.substr(0,s.length - 1) } }; s += vs; while (s[s.length - 1] === ' ') { s = s.substr(0,s.length - 1) }; return s }; for (k in o) { if (Object.hasOwn(o,k)) { l.push(keyValue(k,o[k])) } }; return l.join('\n') }; var toStr = function (o, ind = '', arry = false, seen = []) { var s, t, v; if (!(o != null)) { if (o === null) { return 'null' }; if (o === undefined) { return 'undefined' }; return '<?>' }; switch (t = typeof(o)) { case 'string': {return esc(o,arry)}; case 'object': { if (_k_.in(o,seen)) { return '<v>' }; seen.push(o); if ((o.constructor != null ? o.constructor.name : undefined) === 'Array') { s = ind !== '' && arry && '.' || ''; if (o.length && ind !== '') { s += '\n' }; s += (function () { var result = []; var list = _k_.list(o); for (var li = 0; li < list.length; li++)  { v = list[li];result.push(ind + toStr(v,ind + '    ',true,seen))  } return result }).bind(this)().join('\n') } else if ((o.constructor != null ? o.constructor.name : undefined) === 'RegExp') { return o.source } else { s = (arry && '.\n') || ((ind !== '') && '\n' || ''); s += pretty(o,ind,seen) }; return s } default: return String(o) }; return '<???>' }; return toStr(obj) }, isFunc: function (o) {return typeof o === 'function'}, max: function () { m = -Infinity; for (a of arguments) { if (a instanceof Array) {m = _k_.max.apply(_k_.max,[m].concat(a))} else {n = parseFloat(a); if(!isNaN(n)){m = n > m ? n : m}}}; return m }, in: function (a,l) {return (typeof l === 'string' && typeof a === 'string' && a.length ? '' : []).indexOf.call(l,a) >= 0}}
+var _k_ = {list: function (l) {return l != null ? typeof l.length === 'number' ? l : [] : []}, max: function () { m = -Infinity; for (a of arguments) { if (a instanceof Array) {m = _k_.max.apply(_k_.max,[m].concat(a))} else {n = parseFloat(a); if(!isNaN(n)){m = n > m ? n : m}}}; return m }, noon: function (obj) { var pad = function (s, l) { while (s.length < l) { s += ' ' }; return s }; var esc = function (k, arry) { var es, sp; if (0 <= k.indexOf('\n')) { sp = k.split('\n'); es = sp.map(function (s) { return esc(s,arry) }); es.unshift('...'); es.push('...'); return es.join('\n') } if (k === '' || k === '...' || _k_.in(k[0],[' ','#','|']) || _k_.in(k[k.length - 1],[' ','#','|'])) { k = '|' + k + '|' } else if (arry && /  /.test(k)) { k = '|' + k + '|' }; return k }; var pretty = function (o, ind, seen) { var k, kl, l, v, mk = 4; if (Object.keys(o).length > 1) { for (k in o) { if (Object.hasOwn(o,k)) { kl = parseInt(Math.ceil((k.length + 2) / 4) * 4); mk = Math.max(mk,kl); if (mk > 32) { mk = 32; break } } } }; l = []; var keyValue = function (k, v) { var i, ks, s, vs; s = ind; k = esc(k,true); if (k.indexOf('  ') > 0 && k[0] !== '|') { k = `|${k}|` } else if (k[0] !== '|' && k[k.length - 1] === '|') { k = '|' + k } else if (k[0] === '|' && k[k.length - 1] !== '|') { k += '|' }; ks = pad(k,Math.max(mk,k.length + 2)); i = pad(ind + '    ',mk); s += ks; vs = toStr(v,i,false,seen); if (vs[0] === '\n') { while (s[s.length - 1] === ' ') { s = s.substr(0,s.length - 1) } }; s += vs; while (s[s.length - 1] === ' ') { s = s.substr(0,s.length - 1) }; return s }; for (k in o) { if (Object.hasOwn(o,k)) { l.push(keyValue(k,o[k])) } }; return l.join('\n') }; var toStr = function (o, ind = '', arry = false, seen = []) { var s, t, v; if (!(o != null)) { if (o === null) { return 'null' }; if (o === undefined) { return 'undefined' }; return '<?>' }; switch (t = typeof(o)) { case 'string': {return esc(o,arry)}; case 'object': { if (_k_.in(o,seen)) { return '<v>' }; seen.push(o); if ((o.constructor != null ? o.constructor.name : undefined) === 'Array') { s = ind !== '' && arry && '.' || ''; if (o.length && ind !== '') { s += '\n' }; s += (function () { var result = []; var list = _k_.list(o); for (var li = 0; li < list.length; li++)  { v = list[li];result.push(ind + toStr(v,ind + '    ',true,seen))  } return result }).bind(this)().join('\n') } else if ((o.constructor != null ? o.constructor.name : undefined) === 'RegExp') { return o.source } else { s = (arry && '.\n') || ((ind !== '') && '\n' || ''); s += pretty(o,ind,seen) }; return s } default: return String(o) }; return '<???>' }; return toStr(obj) }, isFunc: function (o) {return typeof o === 'function'}, in: function (a,l) {return (typeof l === 'string' && typeof a === 'string' && a.length ? '' : []).indexOf.call(l,a) >= 0}}
 
 var Board, elem, Game, iconUrl, ogsMoves, Online, open, post, rank, request, slash, WebSocket
 
@@ -94,10 +94,11 @@ Online = (function ()
     {
         var b, g, game, ib, iw, nb, nw, rb, rw
 
+        this.boards = []
         var list = _k_.list(this.activeGames)
-        for (var _89_17_ = 0; _89_17_ < list.length; _89_17_++)
+        for (var _90_17_ = 0; _90_17_ < list.length; _90_17_++)
         {
-            game = list[_89_17_]
+            game = list[_90_17_]
             g = elem('div',{class:'game',parent:this.games})
             if (game.players.black.username !== 'monsterkodi')
             {
@@ -136,14 +137,18 @@ Online = (function ()
     {
         return this.get({path:`/api/v1/games/${game.id}`,cb:(function (g)
         {
-            var b, features, t
+            var b, br, features, rb, t, tb, w
 
             features = {coordinates:false,liberties:false,numbers:false,hover:false}
             b = new Board(e,g.height,features)
             b.game = new Game(b,g.players.black.name,g.players.white.name,g.handicap)
             b.game.replay(ogsMoves(g.gamedata.moves,g.height),true)
-            b.div.style.height = '400px'
-            b.div.style.width = '400px'
+            br = this.parent.getBoundingClientRect()
+            tb = br.height / (this.referee.boardsize + 1) - 2
+            rb = tb
+            w = _k_.max(128,(br.width - br.height) / 2 - tb)
+            b.div.style.height = `${w - 30}px`
+            b.div.style.width = `${w - 30}px`
             b.div.addEventListener('click',((function (g)
             {
                 return (function ()
@@ -151,6 +156,7 @@ Online = (function ()
                     return this.loadGame(g)
                 }).bind(this)
             }).bind(this))(g))
+            this.boards.push(b)
             if (g.gamedata.clock.current_player === 1110858)
             {
                 b.div.style.border = '2px solid black'
@@ -163,17 +169,17 @@ Online = (function ()
 
     Online.prototype["loadGame"] = function (g)
     {
-        var moves, mv, _153_54_
+        var moves, mv, _161_54_
 
         this.referee.newGame({black:g.gamedata.players.black.username,white:g.gamedata.players.white.username,size:g.gamedata.width,handicap:g.gamedata.handicap})
         console.log(_k_.noon(g))
         moves = ogsMoves(g.gamedata.moves,g.gamedata.height)
         this.referee.board.game.replay(moves,true)
         var list = _k_.list(moves)
-        for (var _152_15_ = 0; _152_15_ < list.length; _152_15_++)
+        for (var _160_15_ = 0; _160_15_ < list.length; _160_15_++)
         {
-            mv = list[_152_15_]
-            this.referee.tree.addMove(mv.pos,((_153_54_=mv.captures) != null ? _153_54_ : []),mv.color)
+            mv = list[_160_15_]
+            this.referee.tree.addMove(mv.pos,((_161_54_=mv.captures) != null ? _161_54_ : []),mv.color)
         }
     }
 
@@ -263,7 +269,7 @@ Online = (function ()
 
     Online.prototype["onResize"] = function ()
     {
-        var br, rb, tb, w
+        var b, br, rb, tb, w
 
         if (!this.games)
         {
@@ -272,11 +278,18 @@ Online = (function ()
         br = this.parent.getBoundingClientRect()
         tb = br.height / (this.referee.boardsize + 1) - 2
         rb = tb
-        w = _k_.max(0,(br.width - br.height) / 2 - tb)
+        w = _k_.max(128,(br.width - br.height) / 2 - tb)
         this.games.style.width = `${w}px`
         this.games.style.top = `${tb}px`
         this.games.style.bottom = `${tb}px`
-        return this.games.style.left = `${rb}px`
+        this.games.style.left = `${rb}px`
+        var list = _k_.list(this.boards)
+        for (var _245_14_ = 0; _245_14_ < list.length; _245_14_++)
+        {
+            b = list[_245_14_]
+            b.div.style.width = `${w - 30}px`
+            b.div.style.height = `${w - 30}px`
+        }
     }
 
     return Online
